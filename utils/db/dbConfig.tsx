db6fb440a91aaf9632857a66c3ff93536d0d3f0f
Anzie Anzie <chitrakarnoil@gmail.com>
2024-12-29 22:16:37 +0545

    do the intial next.js project setup and create a schema and push it into the neon wastesnap database

---
+++ utils/db/dbConfig.tsx
@@ -0,0 +1,11 @@
+// postgresql://wastesnap_owner:4SQJwuZEKlU0@ep-soft-bush-a5fqr8gh.us-east-2.aws.neon.tech/wastesnap?sslmode=require
+
+import { neon } from "@neondatabase/serverless";
+
+import { drizzle } from "drizzle-orm/neon-http";
+
+import * as schema from "./schema";
+
+const sql = neon(process.env.DATABASE_URL);
+
+export const db = drizzle(sql, { schema });
